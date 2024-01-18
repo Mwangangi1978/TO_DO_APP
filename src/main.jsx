35579fa5a4003e47865b7c9287a4a4e4caee3dc6
Denis Mwangangi <dennomwangs1978@gmail.com>
2024-01-18 03:19:10 +0300

    chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import customTheme from "./theme"; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={customTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
